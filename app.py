cff77e85fbe94be1c5fc5a102f080cfb819ad977
jhu960213 <jingxujaster@hotmail.com>
2021-02-06 14:25:53 -0500

    some changes

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from flask import request
 import pandas as pd
 
 path = "./covid.csv"
-myData = pd.read_csv(path) # with the default column names
+myData = pd.read_csv(path)  # with the default column names
 
 # flask app
 my_server = flask.Flask(__name__)
 
+
+#
+# safetrek backend API methods to interact with our datasbase
+#
 
 # flask api routing methods
 @my_server.route('/', methods=['GET'])
