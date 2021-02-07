b4c0fe95f5268e2b7f32555d11587bfb3d1bd89a
jhu960213 <jingxujaster@hotmail.com>
2021-02-06 23:02:54 -0500

    finished risk value query function

--- app.py
+++ app.py
@@ -15,29 +15,52 @@ app = flask.Flask(__name__)
 #
 
 # flask api routing methods
-@app.route('/', methods=['GET'])
+@app.route('/risk/eval/', methods=['GET'])
 def query_by_long_lat():
     tempDict = {}
+    num_nearby_locations = 3
     try:
-        lat = request.args.get('lat')
-        long = request.args.get('long')
+        lat = int(request.args['lat'])
+        long = int(request.args['long'])
 
         # storing the sum of abs diff of lat and long and its index value
         for i in range(0, exposure.shape[0]):
             cur_lat = exposure.iloc[i, 2]
             cur_long = exposure.iloc[i, 3]
-            tempDict[abs(cur_lat-lat) + abs(cur_long-long)] = i
+            tempDict[(abs(cur_lat-lat)**2 + abs(cur_long-long)**2)**(1/2)] = i
 
-        # find the min key
-        minKey = min(tempDict.keys())
+        # get a list of the sorted keys
+        keys = list(tempDict.keys())
+        keys.sort()
 
-        # extract desired row from dataframe
-        extracted_row = exposure.iloc[tempDict[minKey], :]
-        return flask.jsonify(extracted_row)
+        # getting the smallest 3 distances from the inputed long and lat
+        min0 = keys[0]
+        min1 = keys[1]
+        min2 = keys[2]
+
+        # extract the 3 closest locations
+        loc0 = exposure.iloc[tempDict[min0], :]
+        loc1 = exposure.iloc[tempDict[min1], :]
+        loc2 = exposure.iloc[tempDict[min2], :]
+        print(loc0)
+        print(loc1)
+        print(loc2)
+
+        # define risk values for each location
+        risk0 = loc0[-1] + loc0[5]
+        risk1 = loc1[-1] + loc1[5]
+        risk2 = loc2[-1] + loc2[5]
+
+        # weighted average risk
+        weight_avg_risk = (risk0*min0 + risk1*min1 + risk2*min2)/num_nearby_locations
+
+        # return a json key value pair
+        return {'weighted_avg_risk': weight_avg_risk}
+
     except Exception as e:
         print(e)
-        return f'Invalid processing!'
+        return f'Invalid processing happened on the backend!'
 
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
