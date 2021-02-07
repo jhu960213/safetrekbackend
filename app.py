951766171cdce9c66352c940820910924945345e
jhu960213 <jingxujaster@hotmail.com>
2021-02-06 20:04:19 -0500

    some changes

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET'])
 def query_all():
     try:
-        return myData.values
+        return myData.to_json()
     except KeyError as e:
         return f'Invalid location!'
